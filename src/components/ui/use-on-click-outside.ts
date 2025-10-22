e18cf5c37489288f78c4d60dba13a6812f984188
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2025-10-22 12:15:40 -0300

    Atualiza o tipo do parâmetro 'ref' na função useOnClickOutside para permitir valores nulos, melhorando a segurança do código ao lidar com referências de elementos.

--- src/components/ui/use-on-click-outside.ts
+++ src/components/ui/use-on-click-outside.ts
@@ -1,7 +1,7 @@
 import { useEffect, RefObject } from 'react'
 
 export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
-  ref: RefObject<T>,
+  ref: RefObject<T | null>,
   handler: (event: MouseEvent | TouchEvent) => void
 ) {
   useEffect(() => {
